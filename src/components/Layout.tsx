fe1cbd63b4ec97a2975ea52ce3b25cdfd558b6bc
Satwik Mishra <150122188+satwikmishra11@users.noreply.github.com>
2025-07-16 17:49:59 +0530

    Fix:details

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,27 +1,19 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Sidebar from './Sidebar'
-import { useAuthStore } from '../store/authStore'
+import React from 'react';
+import Sidebar from './Sidebar';
 
-const Layout: React.FC = () => {
-  const { user } = useAuthStore()
-
-  if (!user) {
-    return <Outlet />
-  }
-
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 ml-64">
-          <div className="max-w-4xl mx-auto py-8 px-6">
-            <Outlet />
-          </div>
-        </main>
-      </div>
-    </div>
-  )
+interface LayoutProps {
+  children: React.ReactNode;
 }
 
-export default Layout
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 p-8 ml-64">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
