bbc8fd6a83f05a2e9c0ac3d0e7f902a76ca561ac
Satwik Mishra <150122188+satwikmishra11@users.noreply.github.com>
2025-07-25 23:09:59 +0530

    Update page.tsx

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -137,14 +137,20 @@ export default function BookmarksPage() {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: index * 0.1 }}
                   >
-                    <Post post={{
-                      ...post,
-                      userId: post.user.id,
-                      username: post.user.username,
-                      fullName: post.user.fullName,
-                      avatar: post.user.avatar,
-                      isVerified: post.user.isVerified,
-                    }} />
+                   <Post
+                      post={{
+                        ...post,
+                        userId: post.user.id,
+                        username: post.user.username,
+                        fullName: post.user.fullName,
+                        avatar: post.user.avatar,
+                        isVerified: post.user.isVerified,
+                      }}
+                      onLike={() => {}}
+                      onComment={() => {}}
+                      onShare={() => {}}
+                      onBookmark={() => {}}
+                    />
                   </motion.div>
                 ))
               ) : (
